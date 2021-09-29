896596481fb741f32789dda748a956d238fd75aa
luisangeles99 <54088660+luisangeles99@users.noreply.github.com>
2021-09-29 13:21:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 import * as THREE from 'https://cdn.skypack.dev/three'
 
+var vertexShaderText = `
+varying vec2 v_uv;
+void main() {
+    v_uv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix *    vec4(position, 1.0);
+}`
+
+var fragmentShaderText = `
+varying vec2 v_uv;
+uniform vec2 u_mouse;
+uniform vec2 u_resolution;
+uniform vec3 u_color;
+uniform float u_time;
+void main() {
+    vec2 v = u_mouse / u_resolution;
+    vec2 uv = gl_FragCoord.xy / u_resolution;
+    gl_FragColor = vec4(1.0, 0.0, sin(u_time * 5.0) + 0.5, 1.0).rgba;
+}`
+
+const material2 = new THREE.ShaderMaterial( {
+
+uniforms: {
+
+    time: { value: 1.0 },
+    resolution: { value: new THREE.Vector2() }
+
+},
+
+vertexShader: vertexShaderText,
+
+fragmentShader: fragmentShaderText
+
+} );
+
 let camera, scene, renderer;
 let geometry, material, mesh;
 
@@ -12,10 +46,10 @@ function init() {
 
     scene = new THREE.Scene();
 
-    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+    geometry = new THREE.BoxGeometry( 0.1, 0.1, 0.1 );
     material = new THREE.MeshNormalMaterial();
 
-    mesh = new THREE.Mesh( geometry, material );
+    mesh = new THREE.Mesh( geometry, material2 );
     scene.add( mesh );
 
     renderer = new THREE.WebGLRenderer( { antialias: true } );
